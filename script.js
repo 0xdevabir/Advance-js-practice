c58ceaede5db020829e2e2df40c60b1a5db61fe2
Abir-ux <abir573912@gmail.com>
2024-12-03 19:59:09 +0600

    done

--- script.js
+++ script.js
@@ -3,10 +3,3 @@ var abir = 'abir';
 
 console.log(abir)
 
-// echo "# Advance-js-practice" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https:/github.com/0xdevabir/Advance-js-practice.git
-// git push -u origin main
